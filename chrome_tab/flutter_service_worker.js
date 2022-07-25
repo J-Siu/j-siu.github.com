2bc25a7973ff092f8eeb2ace08b0c53acb6661c2
John, Sing Dao, Siu <john.sd.siu@gmail.com>
2022-07-25 00:23:11 -0400

    update chrome_tab

--- chrome_tab/flutter_service_worker.js
+++ chrome_tab/flutter_service_worker.js
@@ -5,9 +5,9 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "d428faec2aa434d75fa608ce801a37f2",
 "favicon.ico": "471b0573a60f6195db4cbe2c8719333c",
-"index.html": "e2b8b23aaf97110a19d93600a68e6d79",
-"/": "e2b8b23aaf97110a19d93600a68e6d79",
-"main.dart.js": "702539bdc8686323ef73b55435756cf0",
+"index.html": "706d072c91084eb168086fc68b3df9d2",
+"/": "706d072c91084eb168086fc68b3df9d2",
+"main.dart.js": "1d79fd2c23062b782d1c28a2576f13de",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "favicon.png": "b86780667950fad38001b62fc320194c",
 "icons/Icon-192.png": "29b9dbbe1ebce3861577a38ab278bdc8",
